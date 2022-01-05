9a95794cad21ccdf303a1ed588ef642fe4d6048e
Devous <43449531+Devous@users.noreply.github.com>
2022-01-05 17:43:44 +0000

    Convert SearchResult to record

--- src/dev/devous/searcher/SearchResult.java
+++ src/dev/devous/searcher/SearchResult.java
@@ -1,39 +1,13 @@
 package dev.devous.searcher;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Arrays;
 
-public final class SearchResult {
-    private final String result;
-    private final double similarity;
-    private final String term;
-    private final String[] in;
-
-    SearchResult(final String result, final double similarity,
-            final String term, final String[] in) {
-        this.result = result;
-        this.similarity = similarity;
-        this.term = term;
-        this.in = in;
-    }
-
-    public String result() {
-        return result;
-    }
-
-    public double similarity() {
-        return similarity;
-    }
-
-    public String term() {
-        return term;
-    }
-
-    public String[] in() {
-        return in;
-    }
-
+public final record SearchResult(@NotNull String result, double similarity, @NotNull String term,
+                                 @NotNull String[] in) {
     @Override
-    public String toString() {
+    public @NotNull String toString() {
         return "SearchResult{" +
                 "result='" + result + '\'' +
                 ", similarity=" + similarity +
